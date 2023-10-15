31ba312a4c3fdbaf7b7457cd7a854146e891fcb1
DelStar <delenet@yahoo.com>
2023-10-14 20:24:26 -0400

    update keys

--- app-servers/jenkins-docker/main.tf
+++ app-servers/jenkins-docker/main.tf
@@ -22,7 +22,7 @@ resource "null_resource" "name" {
   connection {
     type        = "ssh"
     user        = "ec2-user"
-    private_key = file("~/Downloads/auto.pem")
+    private_key = file("~/user/auto.pem")
     host        = aws_instance.ec2_instance.public_ip
   }
 
