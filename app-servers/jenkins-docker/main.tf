2f851cbf4923a828c5690053c399cb61d37d18b8
DelStar <31362527+DelStar@users.noreply.github.com>
2023-10-15 10:17:02 -0400

    Update main.tf

--- app-servers/jenkins-docker/main.tf
+++ app-servers/jenkins-docker/main.tf
@@ -1,24 +1,3 @@
-# Added a new null_resource block for copying the script
-resource "null_resource" "copy_script" {
-  triggers = {
-    copy_key = "${null_resource.name.id}"
-  }
-
-  provisioner "file" {
-    source      = "jenkins-docker-script.sh"
-    destination = "/tmp/jenkins-docker-script.sh"
-  }
-
-  provisioner "remote-exec" {
-    inline = [
-      "sudo chmod +x /tmp/jenkins-docker-script.sh",
-      "sh /tmp/jenkins-docker-script.sh",
-    ]
-  }
-
-  depends_on = [aws_instance.ec2_instance]
-}
-
 # Updated comment style for consistency
 # Declaring the AWS Provider
 provider "aws" {
@@ -53,36 +32,37 @@ resource "null_resource" "generate_key" {
       echo "${data.tls_private_key.generated_key.private_key_pem}" > generated_key.pem
     EOT
   }
-}
 
-resource "null_resource" "name" {
-  triggers = {
-    key_generated = "${null_resource.generate_key.*.id}"
+  resource "null_resource" "name" {
+    triggers = {
+      key_generated = "${null_resource.generate_key.*.id}"
+    }
+  
+    # Connection block inside the null_resource block
+    connection {
+      type        = "ssh"
+      user        = "ec2-user"
+      private_key = file("${path.module}/generated_key.pem")
+      host        = aws_instance.ec2_instance.public_ip
+    }
+  
+    # Copy the jenkins-docker-script.sh file from your computer to the ec2 instance
+    provisioner "file" {
+      source      = "jenkins-docker-script.sh"
+      destination = "/tmp/jenkins-docker-script.sh"
+    }
+  
+    # Set permissions and run the install_jenkins.sh file
+    provisioner "remote-exec" {
+      inline = [
+        "sudo chmod +x /tmp/jenkins-docker-script.sh",
+        "sh /tmp/jenkins-docker-script.sh",
+      ]
+    }
+  
+    # Wait for ec2 to be created
+    depends_on = [aws_instance.ec2_instance]
   }
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("${path.module}/generated_key.pem")
-    host        = aws_instance.ec2_instance.public_ip
-  }
-
-  # Copy the jenkins-docker-script.sh file from your computer to the ec2 instance
-  provisioner "file" {
-    source      = "jenkins-docker-script.sh"
-    destination = "/tmp/jenkins-docker-script.sh"
-  }
-
-  # Set permissions and run the install_jenkins.sh file
-  provisioner "remote-exec" {
-    inline = [
-      "sudo chmod +x /tmp/jenkins-docker-script.sh",
-      "sh /tmp/jenkins-docker-script.sh",
-    ]
-  }
-
-  # Wait for ec2 to be created
-  depends_on = [aws_instance.ec2_instance]
 }
 
 # Print the URL of the jenkins server
@@ -90,34 +70,3 @@ output "website_url" {
   value = join("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
 }
 
-
-  connection {
-    type        = "ssh"
-    user        = "ec2-user"
-    private_key = file("${path.module}/generated_key.pem")
-    host        = aws_instance.ec2_instance.public_ip
-  }
-}
-  # copy the jenkins-docker-script.sh file from your computer to the ec2 instance 
-  provisioner "file" {
-    source      = "jenkins-docker-script.sh"
-    destination = "/tmp/jenkins-docker-script.sh"
-  }
-
-  # set permissions and run the install_jenkins.sh file
-  provisioner "remote-exec" {
-    inline = [
-      "sudo chmod +x /tmp/jenkins-docker-script.sh",
-      "sh /tmp/jenkins-docker-script.sh",
-    ]
-  }
-
-  # wait for ec2 to be created
-  depends_on = [aws_instance.ec2_instance]
-}
-
-
-# print the url of the jenkins server
-output "website_url" {
-  value = join("", ["http://", aws_instance.ec2_instance.public_dns, ":", "8080"])
-}
